72c4d20333b89c3269b4b8f012b4166613cb3b8f
Cagdas <74134486+cagdask61@users.noreply.github.com>
2021-02-18 19:47:31 +0300

    18.19 / 18.02.2021

--- Entities/Concrete/Car.cs
+++ Entities/Concrete/Car.cs
@@ -11,9 +11,7 @@ namespace Entities.Concrete
         public int Id { get; set; }
         public int BrandId { get; set; }
         public int ColorId { get; set; }
-        [Required(ErrorMessage ="Araba ismi gereklidir!")]
         public string CarName { get; set; }
-        [Required(ErrorMessage ="Arabnın model yılı girmek zorunludur!")]
         public int ModelYear { get; set; }
         public int DailyPrice { get; set; }
         public string Description { get; set; }
