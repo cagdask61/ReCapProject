bde5b940ffaf11925a4585a9cc9b1dcfdb01f99e
Cagdas <74134486+cagdask61@users.noreply.github.com>
2021-02-27 19:42:47 +0300

    27/02/2021

--- WebAPI/Controllers/CarImagesController.cs
+++ WebAPI/Controllers/CarImagesController.cs
@@ -13,7 +13,7 @@ namespace WebAPI.Controllers
     [ApiController]
     public class CarImagesController : ControllerBase
     {
-       //Başaramadık
+
         ICarImageService _carImageService;
         public CarImagesController(ICarImageService carImageService)
         {
